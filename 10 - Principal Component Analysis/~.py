3104fb35856d1a61a3cf626c3a8d06b6db0f5817
Tejadhith Sankar <tejadhith7@gmail.com>
2023-02-21 17:02:35 +0530

    Initial Commit

---
+++ 10 - Principal Component Analysis/~.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from sklearn.decomposition import PCA
+
+# Read in the data
+df = pd.read_csv(
+    '/Users/tej/Documents/Artificial\ Intelligence/DataSets/Quality\ of\ Wine/wine.csv')
